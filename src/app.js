8e1df51eb08bc58162bc358af662fe35628a155d
lurkyphish2085 <lurkyphish2085@users.noreply.github.com>
2023-10-19 22:36:15 +0800

    added content to the account verification email

--- src/app.js
+++ src/app.js
@@ -31,6 +31,60 @@ router.post('/v1/send-verification', async (req, res) => {
   const subject = "PalmHiram: Account Verification"
 
   const content = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>${subject}</title>
+      <style>
+          body {
+              font-family: 'Arial', sans-serif;
+              margin: 0;
+              padding: 0;
+              background-color: #f4f4f4;
+          }
+
+          .container {
+              max-width: 600px;
+              margin: 0 auto;
+              padding: 20px;
+              background-color: #ffffff;
+              box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+          }
+
+          h1 {
+              color: #333;
+          }
+
+          p {
+              color: #555;
+          }
+      </style>
+  </head>
+
+  <body>
+      <div class="container">
+          <h1>${subject}</h1>
+          <p>Dear ${displayName},</p>
+          <p>Welcome to PalmHiram! You are almost there!</p>
+
+          <p>You already have an account with us, but in order to make business with us,
+             we kindly ask you to complete the verification process by
+             obtaining a verification code from our agent.
+          </p>
+
+          <p>Please contact our verification agent at [Agent Email or Phone] to obtain the code.</p>
+
+          <p>If you have any questions or need assistance, feel free to reply to this email.</p>
+
+          <p>Thank you for choosing PalmHiram!</p>
+          <p>Best regards,<br>PalmHiram</p>
+      </div>
+  </body>
+
+  </html>
   `
 
   let mailTransporter = createTransport({
